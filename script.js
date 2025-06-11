05ccd9fcc9b2bee67c644e27d37c281f7230d0fd
NguyenTan205 <nguyenvantan.utc@gmail.com>
2025-06-11 11:48:37 +0700

    fourth commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    var message = "Hello, world!";
+    console.log(mesage);
+});
+
+function calculateSum(a, b) {
+    return a + b;
+}
+
+calculateSum(5, "10");
